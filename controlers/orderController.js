ec9ca93775ad700c938b4f6817ed6f2be1690038
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2024-05-26 20:20:31 +0530

    Update orderController.js

--- controlers/orderController.js
+++ controlers/orderController.js
@@ -5,7 +5,7 @@ const stripe = new Stripe("sk_test_51PJsNXSJLSHi65wnqzwM0fI9ppYAQZNrSqXlGBi0W30m
 
 //placing user order from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://jagatpurafooddelivery.netlify.app/";
+  const frontend_url = "https://jaipurfooddelivery.netlify.app/";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
